304f1f15355cfeabff0c324673bef483c8f41724
Markus Schanz <coksnuss@googlemail.com>
2019-06-22 01:35:19 +0200

    Pizza Model verbessern

--- frontend/htdocs/js/delivery-service.js
+++ frontend/htdocs/js/delivery-service.js
@@ -5,16 +5,32 @@ const formatPrice = priceAsFloat => priceAsFloat
         currency: 'EUR',
     });
 const pizzalist = [
-    { name: 'Pizzabrot', price: 4.00, imgUrl: '/api/image/pizza?id=1', toppings: 'Mit ohne alles' },
-    { name: 'Margarita', price: 4.50, imgUrl: '/api/image/pizza?id=2', toppings: 'Mit Emmentaler' },
-    { name: 'Mozzarella', price: 6.50, imgUrl: '/api/image/pizza?id=3', toppings: 'Mit Basilikum, Emmentaler und Mozzarella' },
-    { name: 'Fungi', price: 5.30, imgUrl: '/api/image/pizza?id=4', toppings: 'Mit Emmentaler und Pilzen' },
-    { name: 'Gemüse Spezial', price: 9.10, imgUrl: '/api/image/pizza?id=5', toppings: 'Mit Brokkoli, Pilzen, Mozzarella, Oliven, Spinat, Paprika und Tomaten' },
-    { name: 'Gonza', price: 5.40, imgUrl: '/api/image/pizza?id=6', toppings: 'Mit Knoblauch, Gorgonzola und Spinat' },
-    { name: 'Vierkäsezeiten', price: 7.50, imgUrl: '/api/image/pizza?id=7', toppings: 'Mit Schafskäse, Gorgonzola, Gouda und Mozzarella' },
+    { name: 'Pizzabrot', price: 4.00, imgUrl: '/api/image/pizza?id=1', toppings: 'Mit ohne alles', quantity: ko.observable(0) },
+    { name: 'Margarita', price: 4.50, imgUrl: '/api/image/pizza?id=2', toppings: 'Mit Emmentaler', quantity: ko.observable(0) },
+    { name: 'Mozzarella', price: 6.50, imgUrl: '/api/image/pizza?id=3', toppings: 'Mit Basilikum, Emmentaler und Mozzarella', quantity: ko.observable(0) },
+    { name: 'Fungi', price: 5.30, imgUrl: '/api/image/pizza?id=4', toppings: 'Mit Emmentaler und Pilzen', quantity: ko.observable(0) },
+    { name: 'Gemüse Spezial', price: 9.10, imgUrl: '/api/image/pizza?id=5', toppings: 'Mit Brokkoli, Pilzen, Mozzarella, Oliven, Spinat, Paprika und Tomaten', quantity: ko.observable(0) },
+    { name: 'Gonza', price: 5.40, imgUrl: '/api/image/pizza?id=6', toppings: 'Mit Knoblauch, Gorgonzola und Spinat', quantity: ko.observable(0) },
+    { name: 'Vierkäsezeiten', price: 7.50, imgUrl: '/api/image/pizza?id=7', toppings: 'Mit Schafskäse, Gorgonzola, Gouda und Mozzarella', quantity: ko.observable(0) },
 ];
+const add = pizza => {
+    const currentQuantity = pizza.quantity();
+
+    if (currentQuantity < 10) {
+        pizza.quantity(currentQuantity + 1);
+    }
+}
+const remove = pizza => {
+    const currentQuantity = pizza.quantity();
+
+    if (currentQuantity > 0) {
+        pizza.quantity(currentQuantity - 1);
+    }
+}
 
 ko.applyBindings({
     formatPrice: formatPrice,
     pizza: pizzalist,
+    add: add,
+    remove: remove,
 });
